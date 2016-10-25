97254868688c3c3a991843fcacc973c93b366700
Dean Armada <deanarmada@gmail.com>
2016-10-25 23:22:14 +0800

    Added Integration between a project folder and an app

--- project/project/urls.py
+++ project/project/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 
 
 urlpatterns = [
+    # url(r'^sample/', include("sample_app.urls")),
     url(r'^admin/', admin.site.urls),
 ]
 
